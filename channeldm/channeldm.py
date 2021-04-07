960822ae6345393b44c0fcd6959cad9b25a02432
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2021-04-06 22:20:14 -0700

    AWAIT TUPLE???

--- channeldm/channeldm.py
+++ channeldm/channeldm.py
@@ -56,7 +56,7 @@ class ChannelDM(commands.Cog):
         if not message.channel.type == discord.ChannelType.private:
             return
         
-        if await message.content.startswith(await tuple(self.bot.get_valid_prefixes(message.guild))):
+        if await message.content.startswith(tuple(await self.bot.get_valid_prefixes(message.guild))):
             return
 
         message.reply("Yes, I did it!")
