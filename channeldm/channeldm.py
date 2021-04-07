454ac6cbc86401681da8283cb30a624800e80ca6
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2021-04-06 22:05:20 -0700

    Attempting to listen.

--- channeldm/channeldm.py
+++ channeldm/channeldm.py
@@ -5,6 +5,11 @@ from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.config import Config
 
+BASECOG = getattr(commands, "Cog", object)
+DEF_GUILD = {
+    "dump_channel": None
+}
+
 RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
 
@@ -20,12 +25,38 @@ class ChannelDM(commands.Cog):
             identifier="Klypto#3347",
             force_registration=True,
         )
+        self.config.register_guild(**DEF_GUILD)
 
-    @commands.command()
-    async def very_specific_test_command(self, ctx):
-        await ctx.send("I can do stuff.")
-        
 
     async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
         # TODO: Replace this with the proper end user data removal handling.
         super().red_delete_data_for_user(requester=requester, user_id=user_id)
+        return
+
+
+    @commands.guild_only()
+    @commands.is_owner()
+    @commands.command(usage="<channel>")
+    async def set_dump_channel(self, ctx, channel: discord.TextChannel):
+        """
+        Sets the channel where communications will be sent.
+        """
+        await self.config.guild(ctx.guild).dump_channel.set(channel.id)
+        await ctx.send("Done. Set {} as the channel for communications.".format(channel.mention))
+        
+        return
+        
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx, message: discord.Message):
+        """
+        Listens for private messages to forward to the dump channel.
+        """
+        if not message.channel.type == discord.ChannelType.private:
+            return
+        
+        if message.content.startswith(self.config.guild(ctx.guild).get_prefix(message)):
+            return
+
+        message.reply("Yes, I did it!")
+        return
