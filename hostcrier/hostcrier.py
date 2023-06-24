a793f30a113eca62773005bf951c4de4e1422edb
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2023-06-24 15:10:54 -0700

    Slightly better date formatting.

--- hostcrier/hostcrier.py
+++ hostcrier/hostcrier.py
@@ -92,7 +92,7 @@ class HostCrier(commands.Cog):
             embed.description += f"**Message**: " + text
 
         if(creation_info):
-            embed.set_footer(text=datetime.now().astimezone(tz=pytz.timezone("US/Eastern")))
+            embed.set_footer(text=datetime.now().astimezone(tz=pytz.timezone("US/Eastern")).ctime())
 
         return await channel.send(embed=embed)
     
