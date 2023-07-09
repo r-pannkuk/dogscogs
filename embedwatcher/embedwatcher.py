b4d9c15a98019c94ed1330adf546a42d71488b60
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2023-07-09 15:16:44 -0700

    Delays and whitelists.

--- embedwatcher/embedwatcher.py
+++ embedwatcher/embedwatcher.py
@@ -1,5 +1,6 @@
 import collections
 from datetime import timedelta
+import datetime
 from typing import Literal
 import typing
 
@@ -15,6 +16,8 @@ RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 DEFAULT_GUILD = {
     "is_enabled": True,
     "timeout_mins": 10,
+    "delay_mins": 60,
+    "whitelist_channel_ids": [],
     "channel_id": None
 }
 
@@ -79,12 +82,38 @@ class EmbedWatcher(commands.Cog):
         if minutes is None:
             minutes = await self.config.guild(ctx.guild).timeout_mins()
 
+        if minutes < 0:
+            minutes = 0
+
         await self.config.guild(ctx.guild).timeout_mins.set(minutes)
 
         if minutes > 0:
             await ctx.send(f"Users who edit message embeds will be timed out for {minutes} minute{'' if minutes == 1 else 's'}.")
         else:
             await ctx.send(f"Users will not be timed out if they edit embeds.")
+
+        pass
+
+    @embedwatcher.command()
+    @commands.mod_or_permissions(manage_roles=True)
+    async def delay(self, ctx: commands.Context, minutes: typing.Optional[int]):
+        """Defines how long the bot should wait before checking for edits.
+
+        Args:
+            minutes (int): (Optional) How long to delay edit checks for. 
+        """
+        if minutes is None:
+            minutes = await self.config.guild(ctx.guild).delay_mins()
+
+        if minutes < 0:
+            minutes = 0
+
+        await self.config.guild(ctx.guild).delay_mins.set(minutes)
+
+        if minutes > 0:
+            await ctx.send(f"Will wait for {minutes} minute{'' if minutes == 1 else 's'} before checking edited messages.")
+        else:
+            await ctx.send(f"Will not wait to check edited messages.")
 
         pass
 
@@ -116,14 +145,88 @@ class EmbedWatcher(commands.Cog):
 
         pass
 
+    @embedwatcher.group()
+    @commands.mod_or_permissions(manage_roles=True)
+    async def whitelist(self, ctx: commands.Context):
+        """Defines the channel whitelist for edited attachments. 
+        """
+        pass
+
+    @whitelist.command()
+    @commands.mod_or_permissions(manage_roles=True)
+    async def add(self, ctx: commands.Context, channel: discord.TextChannel):
+        """Adds a channel to the white list so it is ignored in scanning attachment changes.
+
+        Args:
+            channel (discord.TextChannel): The discord channel to add. 
+        """
+        whitelist_channel_ids = await self.config.guild(ctx.guild).whitelist_channel_ids()
+        whitelist_channel_ids.append(channel.id)
+        await self.config.guild(ctx.guild).whitelist_channel_ids.set(list(set(whitelist_channel_ids)))
+
+        await ctx.send(f"Added {channel.mention} to the whitelist.")
+        pass
+
+    @whitelist.command()
+    @commands.mod_or_permissions(manage_roles=True)
+    async def remove(self, ctx: commands.Context, channel: discord.TextChannel):
+        """Removes a channel from the white list so it is scanned for attachment changes.
+
+        Args:
+            channel (discord.TextChannel): The discord channel to add. 
+        """
+        whitelist_channel_ids = await self.config.guild(ctx.guild).whitelist_channel_ids()
+        if channel.id not in whitelist_channel_ids:
+            await ctx.send(f"{channel.mention} was not found in the whitelist.")
+            return
+        
+        whitelist_channel_ids.remove(channel.id)
+        await self.config.guild(ctx.guild).whitelist_channel_ids.set(list((set(whitelist_channel_ids))))
+
+        await ctx.send(f"Removed {channel.mention} from the whitelist.")
+        pass
+
+    @whitelist.command()
+    @commands.mod_or_permissions(manage_roles=True)
+    async def list(self, ctx: commands.Context):
+        """Lists all channels currently being ignored in attachment change scans.
+
+        """
+        embed = discord.Embed()
+        embed.title = f"Ignored Channels for Embed / Attachment Scans:"
+
+        channels = []
+        channel_ids = await self.config.guild(ctx.guild).whitelist_channel_ids()
+
+        for i in range(len(channel_ids)):
+            try:
+                channel : discord.TextChannel = await self.bot.fetch_channel(channel_ids[i])
+                channels.append(f"[{i}] {channel.mention}")
+            except:
+                channel_ids.remove(channel_ids[i])
+                continue
+        
+        channels = sorted(channels)
+
+        embed.description = '\n'.join(channels)
+
+        await ctx.send(embed=embed)
+        pass
+
 
     @commands.Cog.listener()
     async def on_raw_message_edit(self, event: discord.RawMessageUpdateEvent):
-
+        if isinstance(event, discord.RawMessageDeleteEvent):
+            return
+        
         guild: discord.Guild = self.bot.get_guild(event.guild_id)
         before = event.cached_message
         after = event.data
-        if "author" in after and "bot" not in after["author"]:
+
+        if "author" not in after or "embeds" not in after or "content" not in after or "attachments" not in after or "edited_timestamp" not in after:
+            return
+        
+        if "bot" not in after["author"]:
             after["author"]["bot"] = False
 
         if before is None:
@@ -134,7 +237,21 @@ class EmbedWatcher(commands.Cog):
                 "content": "",
                 "channel_id": after["channel_id"],
                 "id": after["id"],
+                "created_at": datetime.datetime(year=2000, month=1, day=1, hour=1, minute=1, second=1, microsecond=1)
             })
+        
+        delay_mins = await self.config.guild(guild).delay_mins()
+
+        # ignore delay
+        after_edited_at = datetime.datetime.fromisoformat(after["edited_timestamp"])
+        before_created_at = before.created_at
+        if (after_edited_at.timestamp() - before_created_at.timestamp()) <= timedelta(minutes=delay_mins).total_seconds():
+            return
+        
+        # ignore whitelist
+        whitelist_channel_ids = await self.config.guild(guild).whitelist_channel_ids()
+        if int(after["channel_id"]) in whitelist_channel_ids:
+            return
 
         if await self.config.guild_from_id(guild.id).is_enabled():
             if before.author and before.author.bot:
